e532c7c36a150a115c4baf4688fb314911aadc0d
Yehongjiang <1161963076@qq.com>
2019-02-19 23:21:09 +0800

    New System

---
+++ SewagePlantIMS/SewagePlantIMS/Controllers/ManageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+//using SewagePlantIMS.Models;
+
+namespace SewagePlantIMS.Controllers
+{
+
+    public class ManageController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SewagePlantIMS/SewagePlantIMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SewagePlantIMS.Startup))]
+namespace SewagePlantIMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+        }
+    }
+}
