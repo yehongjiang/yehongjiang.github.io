ade54ff467f0283654394b729435cff0cc8311f8
Yehongjiang <1161963076@qq.com>
2019-02-21 23:28:39 +0800

    update

---
+++ SewagePlantIMS/SewagePlantIMS/Controllers/ElectricManageController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+//using SewagePlantIMS.Models;
+
+namespace SewagePlantIMS.Controllers
+{
+
+    public class ElectricManageController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ElectricalEquipment()
+        {
+            return View();
+        }
+
+    }
+}
