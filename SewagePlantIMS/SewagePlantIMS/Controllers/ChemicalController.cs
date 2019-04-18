debe6d5629704062123a1418b885aa99d6bd58cb
Yehongjiang <1161963076@qq.com>
2019-04-18 22:34:01 +0800

    update

--- SewagePlantIMS/SewagePlantIMS/Controllers/ChemicalController.cs
+++ SewagePlantIMS/SewagePlantIMS/Controllers/ChemicalController.cs
@@ -1,14 +1,19 @@
-﻿using SewagePlantIMS.Models;
+﻿using SewagePlantIMS.Filter;
+using SewagePlantIMS.Function;
+using SewagePlantIMS.Models;
+using SewagePlantIMS.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 namespace SewagePlantIMS.Controllers
 {
+    //[LoginAttribute(isNeed = true)]
     public class ChemicalController : Controller
     {
         // GET: Chemical
@@ -140,6 +145,158 @@ namespace SewagePlantIMS.Controllers
             }
             con.Close();
             return View(models);
+        }
+        public void AddChemicalDevicePicPost()
+        {
+            chemical_device_pic ep = new chemical_device_pic();
+            HttpPostedFileBase ff = Request.Files["File"];
+
+            /*if (ff != null && ff.ContentLength != 0)
+             {
+                 if (!Directory.Exists(Server.MapPath("~images/ElectricalPic")))
+                 {
+                     Directory.CreateDirectory("~images/ElectricalPic");
+                 }
+             }*/
+            //修改文件名去掉其原有的扩展名
+            string filename = Request.Form["filename2"];
+            string[] filename_temp = filename.Split('.');
+            filename = filename_temp[0];
+            //读取elec_pic表最后一个id号加1最为不重复图片名的依据
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["SewagePlantIMS"].ConnectionString);
+            con.Open();
+            string sql = "select max(id) from dm_chemical_device_pic";
+            SqlCommand cmd1 = new SqlCommand(sql, con);
+            int max_id;
+            if (cmd1.ExecuteScalar() != DBNull.Value)
+            {
+                max_id = Convert.ToInt32(cmd1.ExecuteScalar());
+                max_id++;
+            }
+            else
+            {
+                max_id = 1;
+            }
+            string filepath = Server.MapPath("/images/ChemicalDevicePic_Pre/" + filename + "_" + Request.Form["id"] + "_" + max_id.ToString() + ".png");
+            ff.SaveAs(filepath);//在服务器上保存上传原图文件
+                                //string[] readFile = System.IO.File.ReadAllLines(filepath);//读取txt文档存放在字符数组中
+            string filepath2 = Server.MapPath("/images/ChemicalDevicePic/" + filename + "_" + Request.Form["id"] + "_" + max_id.ToString() + ".png");
+            CompressPic cp = new CompressPic();
+            bool temp = cp.CompressImage(filepath, filepath2, 80, 150, true); //保存压缩完的文件到ChemicalDevicePic文件夹
+
+
+            string sqlStr = "insert into dm_chemical_device_pic (cd_picname,cd_picurl,cd_addtime,cd_id) values('" + filename + "','" + "/images/ChemicalDevicePic/" + filename + "_" + Request.Form["id"] + "_" + max_id.ToString() + ".png" + "','" + DateTime.Now.ToString() + "','" + Request.Form["id"] + "')";
+            SqlCommand cmd = new SqlCommand(sqlStr, con);
+            int check = cmd.ExecuteNonQuery();
+            if (check == 1)
+            {
+                Response.Write("<script>alert('添加图片成功！！');window.location.href='AddChemicalDevicePic?id=" + Request.Form["id"] + " '   ;</script>");
+            }
+            else
+                Response.Write("<script>alert('添加图片失败！,请手动联系管理员~');</script>");
+            con.Close();
+            //return RedirectToAction("Electrical_List"); //如果直接View就不会执行HttpGet的代码
+            //return ElectricalPic();
+        }
+        public void DeleteChemicalDevicePic()
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["SewagePlantIMS"].ConnectionString);
+            con.Open();
+            //先查出url
+            string pic_url = "select cd_picurl from dm_chemical_device_pic  where id =  '" + Request.Form["del_id"] + "'";
+            SqlCommand cmd_url = new SqlCommand(pic_url, con);
+            pic_url = cmd_url.ExecuteScalar().ToString();
+            string sql = "select cd_id from dm_chemical_device_pic  where id =  '" + Request.Form["del_id"] + "'";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            int id = Convert.ToInt32(cmd.ExecuteScalar());
+            string sqlStr = "delete from dm_chemical_device_pic where id =  '" + Request.Form["del_id"] + "'";
+            SqlCommand cmd2 = new SqlCommand(sqlStr, con);
+            int check = cmd2.ExecuteNonQuery();
+            con.Close();
+            //删除对应的实体文件
+            string filePath = Server.MapPath(pic_url);//路径 
+            FileInfo file = new FileInfo(filePath);
+            if (file.Exists)
+            {
+                file.Delete();
+            }
+            //删除原图
+            string preurl = pic_url.Insert(25,"_Pre");
+            filePath = Server.MapPath(preurl);//路径 
+            file = new FileInfo(filePath);
+            if (file.Exists)
+            {
+                file.Delete();
+            }
+
+            if (check == 1)
+            {
+                Response.Write("<script>alert('删除图片成功！！');window.location.href='AddChemicalDevicePic?id=" + id + " '   ;</script>");
+            }
+            else
+                Response.Write("<script>alert('删除失败！,请手动联系管理员~');</script>");
+        }
+        //化验设备展示页
+        [LoginAttribute(isNeed = false)]
+        [HttpGet]
+        public ActionResult ShowChemicalDevice(string id)
+        {
+            List<ShowChemicalDevice> model = new List<ShowChemicalDevice>();
+
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["SewagePlantIMS"].ConnectionString);
+            con.Open();
+            string sqlStr = "select dm_chemical_device.id,cd_name,cd_model,cd_quantity,cd_num,cd_manufacturer,cd_preserver,cd_begin_time,cd_remark ,cd_picurl from dm_chemical_device,dm_chemical_device_pic where dm_chemical_device.id = '" + Request.Form["show"] + "'and  dm_chemical_device.id = dm_chemical_device_pic.cd_id; ";
+            SqlDataAdapter da = new SqlDataAdapter(sqlStr, con);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            ShowChemicalDevice[] E = new ShowChemicalDevice[ds.Tables[0].Rows.Count];
+            for (int mDr = 0; mDr < ds.Tables[0].Rows.Count; mDr++)
+            {
+                E[mDr] = new ShowChemicalDevice();
+                E[mDr].id = Convert.ToInt32(ds.Tables[0].Rows[mDr][0]);
+                E[mDr].cd_name = ds.Tables[0].Rows[mDr][1].ToString();
+                E[mDr].cd_model = ds.Tables[0].Rows[mDr][2].ToString();
+                E[mDr].cd_quantity = Convert.ToInt32(ds.Tables[0].Rows[mDr][3]);
+                E[mDr].cd_num = ds.Tables[0].Rows[mDr][4].ToString();
+                E[mDr].cd_manufacturer = ds.Tables[0].Rows[mDr][5].ToString();
+                E[mDr].cd_preserver = ds.Tables[0].Rows[mDr][6].ToString();
+                E[mDr].cd_begin_time = Convert.ToDateTime(ds.Tables[0].Rows[mDr][7]);
+                E[mDr].cd_remark = ds.Tables[0].Rows[mDr][8].ToString();
+                E[mDr].cd_picurl = ds.Tables[0].Rows[mDr][9].ToString();
+                model.Add(E[mDr]);
+            }
+            con.Close();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult ShowChemicalDevice()
+        {
+            List<ShowChemicalDevice> model = new List<ShowChemicalDevice>();
+
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["SewagePlantIMS"].ConnectionString);
+            con.Open();
+            string sqlStr = "select dm_chemical_device.id,cd_name,cd_model,cd_quantity,cd_num,cd_manufacturer,cd_preserver,cd_begin_time,cd_remark ,cd_picurl from dm_chemical_device,dm_chemical_device_pic where dm_chemical_device.id = '" + Request.Form["show"] + "'and  dm_chemical_device.id = dm_chemical_device_pic.cd_id; ";
+            SqlDataAdapter da = new SqlDataAdapter(sqlStr, con);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            ShowChemicalDevice[] E = new ShowChemicalDevice[ds.Tables[0].Rows.Count];
+            for (int mDr = 0; mDr < ds.Tables[0].Rows.Count; mDr++)
+            {
+                E[mDr] = new ShowChemicalDevice();
+                E[mDr].id = Convert.ToInt32(ds.Tables[0].Rows[mDr][0]);
+                E[mDr].cd_name = ds.Tables[0].Rows[mDr][1].ToString();
+                E[mDr].cd_model = ds.Tables[0].Rows[mDr][2].ToString();
+                E[mDr].cd_quantity = Convert.ToInt32(ds.Tables[0].Rows[mDr][3]);
+                E[mDr].cd_num = ds.Tables[0].Rows[mDr][4].ToString();
+                E[mDr].cd_manufacturer = ds.Tables[0].Rows[mDr][5].ToString();
+                E[mDr].cd_preserver = ds.Tables[0].Rows[mDr][6].ToString();
+                E[mDr].cd_begin_time = Convert.ToDateTime(ds.Tables[0].Rows[mDr][7]);
+                E[mDr].cd_remark = ds.Tables[0].Rows[mDr][8].ToString();
+                E[mDr].cd_picurl = ds.Tables[0].Rows[mDr][9].ToString();
+                model.Add(E[mDr]);
+            }
+            con.Close();
+            return View(model);
         }
     }
 }
